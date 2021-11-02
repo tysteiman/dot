8aab8e0ad8ec29195791bee43bd3628c8da1132d
Tyler Steiman <tsteiman@switchboxinc.com>
2021-11-01 21:24:52 -0400

    updated install script -- add emacs2

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/plexus/chemacs2 ~/.emacs.d
 
 ln -s $PWD/.ctags ~/
-ln -s $PWD/.emacs ~/
 ln -s $PWD/.gitconfig ~/
 ln -s $PWD/.nanorc ~/
 ln -s $PWD/.tmux.conf ~/
