ac6819bf2cfea4e1deef0c6a75dff29c7ae4e309
Tyler Steiman <tsteiman@switchboxinc.com>
2021-11-04 10:50:07 -0400

    Add emacs doom profile

--- .emacs.d/.emacs-profiles.el
+++ .emacs.d/.emacs-profiles.el
@@ -1,3 +1,4 @@
 (("default"   . ((user-emacs-directory . "~/dot/.emacs.d/")))
  ("spacemacs" . ((user-emacs-directory . "~/.emacs-configs/spacemacs/")))
+ ("doom"      . ((user-emacs-directory . "~/.emacs-configs/doom/")))
  ("vanilla"   . ((user-emacs-directory . "~/.emacs-configs/vanilla/"))))
