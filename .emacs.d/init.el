22467bcc4e35d771ca79c48caacba3dd98687d75
Tyler Steiman <tsteiman@switchboxinc.com>
2021-11-05 13:29:17 -0400

    kbd for whitespace cleanup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -112,6 +112,7 @@
 (global-set-key (kbd "C-x C-c") 'my/quit-emacs)
 (global-set-key (kbd "C-o")     'my/new-next-line)
 (global-set-key (kbd "M-z")     'zap-up-to-char)
+(global-set-key (kbd "C-c w w") 'whitespace-cleanup)
 
 ;; INSTALL PACKAGES (use-package declarations)
 (require 'package)
