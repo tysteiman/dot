ae9966d397cf17407af005779f27ee5986cda5ff
Tyler Steiman <tsteiman@switchboxinc.com>
2021-11-09 14:21:34 -0500

    update install stuff

--- install.sh
+++ install.sh
@@ -8,4 +8,6 @@ ln -s $PWD/.gitconfig ~/
 ln -s $PWD/.nanorc ~/
 ln -s $PWD/.tmux.conf ~/
 ln -s $PWD/.vimrc ~/
+ln -s $PWD/.xinitrc ~/
 ln -s $PWD/.emacs.d/emacs-profiles.el ~/
+ln -s $PWD/i3 ~/.config/i3
