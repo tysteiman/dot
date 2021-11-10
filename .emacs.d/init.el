b2a419b195a4a71b3ecc5d0b4bd8bc5b053b66e7
Tyler Steiman <tsteiman@switchboxinc.com>
2021-11-10 13:06:49 -0500

    height update for i3

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,7 @@
 ;; needed this on wsl, not on linux
 ;; (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
-(set-face-attribute 'default nil :height 110)
+(set-face-attribute 'default nil :height 80)
 
 (tool-bar-mode 0)
 (menu-bar-mode 0)
