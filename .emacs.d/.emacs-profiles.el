cc8956987d446238f8d0ad7e25fe39e4955ed52b
Tyler Steiman <tsteiman@switchboxinc.com>
2022-01-18 16:06:13 -0500

    remove chemacs stuff

--- .emacs.d/.emacs-profiles.el
+++
@@ -1,4 +0,0 @@
-(("default"   . ((user-emacs-directory . "~/dot/.emacs.d/")))
- ("spacemacs" . ((user-emacs-directory . "~/.emacs-configs/spacemacs/")))
- ("doom"      . ((user-emacs-directory . "~/.emacs-configs/doom/")))
- ("base"      . ((user-emacs-directory . "~/.emacs-configs/base/"))))
--- install.sh
+++ install.sh
@@ -3,20 +3,17 @@
 # install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# install chemacs
-git clone https://github.com/plexus/chemacs2 ~/.emacs.d
-
 if [ ! -d ~/.config ]; then
     mkdir ~/.config
 fi
 
+ln -s $PWD/.emacs.d ~/
 ln -s $PWD/.ctags ~/
 ln -s $PWD/.gitconfig ~/
 ln -s $PWD/.nanorc ~/
 ln -s $PWD/.tmux.conf ~/
 ln -s $PWD/.vimrc ~/
 ln -s $PWD/.xinitrc ~/
-ln -s $PWD/.emacs.d/.emacs-profiles.el ~/
 ln -s $PWD/i3 ~/.config/
 ln -s $PWD/kitty ~/.config/
 ln -s $PWD/polybar ~/.config/
