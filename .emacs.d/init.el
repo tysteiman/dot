625cf55f016bf428342f8c04c4c6dbba06529d4a
Tyler Steiman <tsteiman@switchboxinc.com>
2022-03-08 16:36:20 -0500

    toying with the idea of compiling emacs configs instead

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,33 @@
-(require 'org)
+(defun my/compile-config ()
+  "Compile init.org into init.elc"
+  (interactive)
+  (let ((source-file (concat user-emacs-directory "init.org"))
+        (target-file (concat user-emacs-directory "init-tangle.el")))
+    (require 'org)
+    (org-babel-tangle-file source-file target-file)
+    (byte-compile-file target-file)))
 
-(let ((source-file (concat user-emacs-directory "init.org"))
-      (target-file (concat user-emacs-directory "init-tangle.el")))
-  (org-babel-tangle-file source-file target-file)
-  (load-file target-file))
+(defun my/setup-config-auto-compile-hook ()
+  (let ((config (concat user-emacs-directory "init.org")))
+    (when (string-equal (expand-file-name (buffer-file-name)) (expand-file-name config))
+      (add-hook 'after-save-hook 'my/compile-config))))
+
+(let ((init (concat user-emacs-directory "init-tangle.elc")))
+  (if (file-exists-p init)
+      (load-file init)
+    (progn
+      (my/compile-config)
+      (load-file init)))
+  (add-hook 'org-mode-hook 'my/setup-config-auto-compile-hook))
+
+
+
+
+
+
+;; (require 'org)
+;; 
+;; (let ((source-file (concat user-emacs-directory "init.org"))
+;;       (target-file (concat user-emacs-directory "init-tangle.el")))
+;;   (org-babel-tangle-file source-file target-file)
+;;   (load-file target-file))
