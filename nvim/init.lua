f6781dc57e9fa363d050676d27b4dd52e45a0c46
Tyler Steiman <tsteiman@switchboxinc.com>
2022-05-17 13:06:36 -0400

    more telescope bindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,8 +24,11 @@ map('n', '<c-k>', ':wincmd k<CR>', silentnoremap)
 map('n', '<c-j>', ':wincmd j<CR>', silentnoremap)
 map('n', '<c-h>', ':wincmd h<CR>', silentnoremap)
 map('n', '<c-l>', ':wincmd l<CR>', silentnoremap)
-map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', silentnoremap)
+map('n', '<leader>ff', '<cmd>Telescope git_files<cr>', silentnoremap)
 map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', silentnoremap)
+map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', silentnoremap)
+map('n', '<leader>fc', '<cmd>Telescope git_commits<cr>', silentnoremap)
+map('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>', silentnoremap)
 map('n', '<leader>q', ':bnext<cr>', silentnoremap)
 map('n', '<leader>a', ':bprevious<cr>', silentnoremap)
 
@@ -45,15 +48,13 @@ require('packer').startup(function()
     use 'prettier/vim-prettier'
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     -- use 'neovim/nvim-lspconfig'
-    -- use 'kabouzeid/nvim-lspinstall' -- Install LSP servers on demand with :LSPInstall <name_of_language>
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
-    use 'L3MON4D3/LuaSnip'
-    use 'saadparwaiz1/cmp_luasnip'
+    -- use 'hrsh7th/cmp-nvim-lsp'
+    -- use 'hrsh7th/cmp-buffer'
+    -- use 'hrsh7th/cmp-path'
+    -- use 'hrsh7th/cmp-cmdline'
+    -- use 'hrsh7th/nvim-cmp'
+    -- use 'L3MON4D3/LuaSnip'
+    -- use 'saadparwaiz1/cmp_luasnip'
 end)
 
 require'nvim-treesitter.configs'.setup {
@@ -73,72 +74,72 @@ vim.g['prettier#autoformat_config_present'] = 1
 vim.g['prettier#autoformat_require_pragma'] = 0
 
 -- Setup nvim-cmp.
-local cmp = require'cmp'
-
-cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        end,
-    },
-    window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'vsnip' }, -- For vsnip users.
-        -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
-    }, {
-            { name = 'buffer' },
-        })
-})
-
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-            { name = 'buffer' },
-        })
-})
-
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = 'buffer' }
-    }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-            { name = 'cmdline' }
-        })
-})
-
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['solargraph'].setup {
-    capabilities = capabilities
-}
+-- local cmp = require'cmp'
+-- 
+-- cmp.setup({
+--     snippet = {
+--         -- REQUIRED - you must specify a snippet engine
+--         expand = function(args)
+--             -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+--             require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+--             -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+--             -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+--         end,
+--     },
+--     window = {
+--         -- completion = cmp.config.window.bordered(),
+--         -- documentation = cmp.config.window.bordered(),
+--     },
+--     mapping = cmp.mapping.preset.insert({
+--         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+--         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--         ['<C-Space>'] = cmp.mapping.complete(),
+--         ['<C-e>'] = cmp.mapping.abort(),
+--         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+--     }),
+--     sources = cmp.config.sources({
+--         { name = 'nvim_lsp' },
+--         { name = 'vsnip' }, -- For vsnip users.
+--         -- { name = 'luasnip' }, -- For luasnip users.
+--         -- { name = 'ultisnips' }, -- For ultisnips users.
+--         -- { name = 'snippy' }, -- For snippy users.
+--     }, {
+--             { name = 'buffer' },
+--         })
+-- })
+-- 
+-- -- Set configuration for specific filetype.
+-- cmp.setup.filetype('gitcommit', {
+--     sources = cmp.config.sources({
+--         { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+--     }, {
+--             { name = 'buffer' },
+--         })
+-- })
+-- 
+-- -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline('/', {
+--     mapping = cmp.mapping.preset.cmdline(),
+--     sources = {
+--         { name = 'buffer' }
+--     }
+-- })
+-- 
+-- -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline(':', {
+--     mapping = cmp.mapping.preset.cmdline(),
+--     sources = cmp.config.sources({
+--         { name = 'path' }
+--     }, {
+--             { name = 'cmdline' }
+--         })
+-- })
+-- 
+-- -- Setup lspconfig.
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+-- require('lspconfig')['solargraph'].setup {
+--     capabilities = capabilities
+-- }
 
 
