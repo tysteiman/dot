605e77a1b8e34a1bfcb4f4c4ae4e570fd07c04c9
Tyler Steiman <tsteiman@switchboxinc.com>
2022-07-14 16:33:20 -0400

    get rid of emacs profiles file

--- .emacs.d/.emacs-profiles.el
+++
@@ -1,2 +0,0 @@
-(("default" . ((user-emacs-directory . "~/dot/.emacs.d")))
- ("doom"    . ((user-emacs-directory . "~/.emacs-profiles/doom"))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -238,23 +238,23 @@
 (use-package slim-mode
   :defer t)
 
-(use-package lua-mode
-  :defer t)
+;; (use-package lua-mode
+;;   :defer t)
 
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  (setq lsp-headerline-breadcrumb-enable nil)
-  :config
-  (lsp-enable-which-key-integration t)
-  :hook
-  (js2-mode  . lsp-deferred)
-  (rjsx-mode . lsp-deferred)
-  (php-mode  . lsp-deferred))
+;; (use-package lsp-mode
+;;   :commands (lsp lsp-deferred)
+;;   :init
+;;   (setq lsp-keymap-prefix "C-c l")
+;;   (setq lsp-headerline-breadcrumb-enable nil)
+;;   :config
+;;   (lsp-enable-which-key-integration t)
+;;   :hook
+;;   (js2-mode  . lsp-deferred)
+;;   (rjsx-mode . lsp-deferred)
+;;   (php-mode  . lsp-deferred))
 
-(use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode))
+;; (use-package lsp-ui
+;;   :hook (lsp-mode . lsp-ui-mode))
 
 (use-package company
   :init
