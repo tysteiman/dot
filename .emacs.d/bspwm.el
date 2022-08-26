be8aa9d0e2161dc86e0e130260e14bee40090763
Tyler Steiman <tsteiman@switchboxinc.com>
2022-08-25 20:17:48 -0400

    bspwm.el

---
+++ .emacs.d/bspwm.el
@@ -0,0 +1,31 @@
+(defun bspc (command)
+  "Run a command via bspc"
+  (start-process-shell-command "bspc" nil (concat "bspc " command)))
+
+(defun bspc-config (command)
+  "Run a bspc config command"
+  (bspc (concat "config " command)))
+
+(defun bspc-monitor (command)
+  "Run a bspc monitor command"
+  (bspc (concat "monitor " command)))
+
+(defun bspc-rule (command)
+  "Run a bspc rule command"
+  (bspc (concat "rule " command)))
+
+(bspc-monitor "eDP-1 -r")
+(bspc-monitor "HDMI-1-0 -d I II III IV V")
+
+(bspc-config "focused_border_color '#FFCB6B'")
+(bspc-config "border_width 2")
+(bspc-config "window_gap 15")
+(bspc-config "split_ratio 0.52")
+(bspc-config "borderless_monocle true")
+(bspc-config "gapless_monocle true")
+
+(bspc-rule "-a Chromium desktop='^2'")
+(bspc-rule "-a mplayer2 state=floating")
+(bspc-rule "-a Kupfer.py focus=on")
+(bspc-rule "-a Screenkey manage=off")
+(bspc-rule "-a Emacs state=tiled")
