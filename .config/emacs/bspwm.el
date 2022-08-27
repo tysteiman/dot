c3b74c033c8ced4e9513a37a5659abbddf4fbb5d
Tyler Steiman <tsteiman@switchboxinc.com>
2022-08-27 00:43:52 -0400

    move bspwm configs into bspwm.el, load if bswpm process is running

---
+++ .config/emacs/bspwm.el
@@ -0,0 +1,40 @@
+(defun bspc (cmd-group command)
+  "Run command(s) via bspc.
+
+CMD-GROUP is the group that bspc will use e.g. config, monitor, rule.
+
+COMMAND is either a string of a command to run, or a list of strings."
+  (let ((command-set (if (listp command)
+                         command
+                       (list command))))
+    (dolist (cmd command-set)
+      (start-process-shell-command "bspc" nil (concat "bspc " cmd-group " " cmd)))))
+
+(defun bspc-config (command)
+  "Run a bspc config command"
+  (bspc "config" command))
+
+(defun bspc-monitor (command)
+  "Run a bspc monitor command"
+  (bspc "monitor" command))
+
+(defun bspc-rule (command)
+  "Run a bspc rule command"
+  (bspc "rule -a" command))
+
+;; fire bspwm configs
+(bspc-monitor '("eDP-1 -r"
+                "HDMI-1-0 -d I II III IV V"))
+
+(bspc-config '("focused_border_color '#FFCB6B'"
+               "border_width 2"
+               "window_gap 15"
+               "split_ratio 0.52"
+               "borderless_monocle true"
+               "gapless_monocle true"))
+
+(bspc-rule '("Chromium desktop='^2'"
+             "mplayer2 state=floating"
+             "Kupfer.py focus=on"
+             "Screenkey manage=off"
+             "Emacs state=tiled"))
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -2,6 +2,13 @@
 
 (setq gc-cons-threshold (* 60 1000 1000))
 
+;; Load bspwm configs if bspwm process is running.
+(dolist (pc (list-system-processes))
+  (let* ((psattr (process-attributes pc))
+         (psname (cdr (assoc 'comm psattr))))
+    (when (string= psname "bspwm")
+      (load-file (concat user-emacs-directory "bspwm.el")))))
+
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
@@ -570,47 +577,6 @@
 (setq gc-cons-threshold 100000000)
 
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
-
-(defun bspc (cmd-group command)
-  "Run command(s) via bspc.
-
-CMD-GROUP is the group that bspc will use e.g. config, monitor, rule.
-
-COMMAND is either a string of a command to run, or a list of strings."
-  (let ((command-set (if (listp command)
-                         command
-                       (list command))))
-    (dolist (cmd command-set)
-      (start-process-shell-command "bspc" nil (concat "bspc " cmd-group " " cmd)))))
-
-(defun bspc-config (command)
-  "Run a bspc config command"
-  (bspc "config" command))
-
-(defun bspc-monitor (command)
-  "Run a bspc monitor command"
-  (bspc "monitor" command))
-
-(defun bspc-rule (command)
-  "Run a bspc rule command"
-  (bspc "rule -a" command))
-
-;; fire bspwm configs
-(bspc-monitor '("eDP-1 -r"
-                "HDMI-1-0 -d I II III IV V"))
-
-(bspc-config '("focused_border_color '#FFCB6B'"
-               "border_width 2"
-               "window_gap 15"
-               "split_ratio 0.52"
-               "borderless_monocle true"
-               "gapless_monocle true"))
-
-(bspc-rule '("Chromium desktop='^2'"
-             "mplayer2 state=floating"
-             "Kupfer.py focus=on"
-             "Screenkey manage=off"
-             "Emacs state=tiled"))
 
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
