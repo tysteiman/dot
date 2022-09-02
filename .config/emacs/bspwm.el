89fb4576679c97e4a4cb238841b2479c0521f721
Tyler Steiman <tsteiman@switchboxinc.com>
2022-09-02 16:08:31 -0400

    single quote bspwm.el colors

--- .config/emacs/bspwm.el
+++ .config/emacs/bspwm.el
@@ -27,8 +27,8 @@ COMMAND is either a string of a command to run, or a list of strings."
 (bspc-monitor '((eDP-1    . "-r")
                 (HDMI-1-0 . "-d I II III IV V VI")))
 
-(bspc-config '((focused_border_color  . "#ffcb6b")
-               (presel_feedback_color . "#717CB4")
+(bspc-config '((focused_border_color  . "'#ffcb6b'")
+               (presel_feedback_color . "'#717CB4'")
                (border_width          . 1)
                (window_gap            . 100)
                (split_ratio           . 0.60)
