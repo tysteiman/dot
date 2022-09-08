175a2b9761304dabc3f334ba3971d02489bc57aa
Tyler Steiman <tsteiman@switchboxinc.com>
2022-09-08 10:49:13 -0400

    initial install script for installing arch system

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# INSTALL BASE PACKAGES NEEDED (Defaults to my bspwm setup...)
+yay -S emacs                  \
+    stow                      \
+    firefox                   \
+    rofi                      \
+    pavucontrol               \
+    pulseaudio                \
+    bspwm                     \
+    sxhkd                     \
+    picom                     \
+    fish                      \
+    nerd-fonts-jetbrains-mono \
+    kitty                     \
+    pcmanfm                   \
+    dunst                     \
+    libnotify                 \
+    feh                       \
+    polybar                   \
+    ripgrep                   \
+    sudo                      \
+    openssh                   \
+    slack-desktop             \
+    flameshot
+
+# make config directory if it doesnt exist
+CONF=~/.config
+if [ ! -d $CONF ]; then mkdir $CONF; fi
+
+# move bashrc around if it exists
+if [ -f ~/.bashrc ]; then mv ~/.bashrc ~/.bashrc.original; fi
+
+# run stow!
+stow -v .
