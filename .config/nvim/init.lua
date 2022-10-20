46601ae5dc77d2da826ee5c06cc9c73480068bea
Tyler Steiman <tsteiman@switchboxinc.com>
2022-10-20 09:56:24 -0400

    small updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -58,7 +58,7 @@ require'nvim-treesitter.configs'.setup {
     sync_install = false,
     highlight = {
         enable = true,
-        disable = { "php"} -- right now the php syntax is broken for some reason...
+        disable = {"php"} -- right now the php syntax is broken for some reason...
     }
 }
 
