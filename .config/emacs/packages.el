3b02f4c7dfc380c5849bbdd649b0966143a4702d
Tyler Steiman <tsteiman@switchboxinc.com>
2022-12-01 10:49:06 -0500

    mu4e C-c m m

--- .config/emacs/packages.el
+++ .config/emacs/packages.el
@@ -342,7 +342,8 @@
   (setq mu4e-drafts-folder "/[Gmail].Drafts")
   (setq mu4e-sent-folder "/[Gmail].Sent Mail")
   (setq mu4e-refile-folder "/[Gmail].All Mail")
-  (setq mu4e-trash-folder "/[Gmail].Trash"))
+  (setq mu4e-trash-folder "/[Gmail].Trash")
+  :bind (("C-c m m" . mu4e)))
 
 (use-package mu4e-alert
   :after mu4e
