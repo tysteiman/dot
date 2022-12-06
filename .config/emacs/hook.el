ef1b3b8b4c5aafd297a92f81c8f89118324a8337
Tyler Steiman <tsteiman@switchboxinc.com>
2022-12-05 23:01:34 -0500

    add hook for dired hide details, use by default

--- .config/emacs/hook.el
+++ .config/emacs/hook.el
@@ -4,3 +4,20 @@
 
 (eval-after-load 'ruby-mode '(progn
                                (define-key ruby-mode-map (kbd "C-c r r") 'my/async-shell-command-rubocop)))
+
+(defvar my/dired-hide-details 1
+  "Whether or not to show dired details")
+
+;; dired hook
+(add-hook 'dired-mode-hook (lambda ()
+                             (interactive)
+                             ;; launch hide details mode based on current value
+                             (dired-hide-details-mode my/dired-hide-details)
+                             ;; set keybinding to toggle that hide details value
+                             (define-key dired-mode-map (kbd "C-c d d") (lambda ()
+                                                                          (interactive)
+                                                                          (let ((newval (if (eql my/dired-hide-details 0)
+                                                                                            1
+                                                                                          0)))
+                                                                            (dired-hide-details-mode newval)
+                                                                            (setq my/dired-hide-details newval))))))
