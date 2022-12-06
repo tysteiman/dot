16ceface11610ea3f765044308da190bfa3bf2d7
Tyler Steiman <tsteiman@switchboxinc.com>
2022-12-05 23:03:08 -0500

    remove extra eshell packages

--- .config/emacs/packages.el
+++ .config/emacs/packages.el
@@ -271,13 +271,6 @@
   :init (setq-default evil-escape-key-sequence "jk")
   :config (evil-escape-mode))
 
-(use-package eshell-info-banner
-  :defer t
-  :hook (eshell-banner-load . eshell-info-banner-update-banner))
-
-(use-package eshell-git-prompt
-  :config (eshell-git-prompt-use-theme 'git-radar))
-
 (use-package vterm
   :init (setq vterm-shell "/usr/bin/fish")
   :bind (("C-c e v" . my/vterm)
