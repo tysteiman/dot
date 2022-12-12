5e93fd99c4ef004eb1b8c32625ecab22a2194797
Tyler Steiman <tsteiman@switchboxinc.com>
2022-12-11 22:44:54 -0500

    use feather dark in emacs

--- .config/emacs/packages.el
+++ .config/emacs/packages.el
@@ -160,12 +160,11 @@
 (use-package hl-todo
   :hook (prog-mode . hl-todo-mode))
 
-(defvar my/theme 'doom-one
+(defvar my/theme 'doom-feather-dark
   "Theme to use")
 
 (use-package doom-themes
-  :defer t
-  ;; :config (load-theme 'doom-palenight t)
+  :config (load-theme my/theme t)
   :hook (server-after-make-frame . (lambda ()
                                      (load-theme my/theme t))))
 
