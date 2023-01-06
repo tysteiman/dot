55812a8d4f3329f3e51062fac04951f796ba11bd
Tyler Steiman <tsteiman@switchboxinc.com>
2023-01-06 16:56:42 -0500

    more doom settings

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -12,6 +12,7 @@
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
 ;; - `doom-font' -- the primary font to use
+(setq doom-font "JetBrainsMono Nerd Font-11")
 ;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
 ;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
@@ -41,6 +42,10 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+
+(setq-default c-basic-offset 4)
+(setq-default js-indent-level 4)
+(setq-default sgml-basic-offset 4)
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
