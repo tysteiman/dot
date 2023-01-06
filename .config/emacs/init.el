1abddcd7f712212c639b6bb796211927167465a2
Tyler Steiman <tsteiman@switchboxinc.com>
2023-01-06 16:56:52 -0500

    comment out doom modeline -- breaking emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -428,20 +428,20 @@
 (use-package all-the-icons
   :defer t)
 
-(use-package doom-modeline
-  :init
-  (setq doom-modeline-height 50
-        doom-modeline-bar-width 5
-        doom-modeline-vcs-max-length 25
-        doom-modeline-buffer-file-name-style "file-name")
-  :config
-  (doom-modeline-mode 1)
-  ;; (set-face-attribute 'doom-modeline-time nil :foreground "#e0af68")
-  ;; (set-face-attribute 'doom-modeline-evil-normal-state nil :background "#7aa2f7" :foreground "black")
-  ;; (set-face-attribute 'doom-modeline-evil-insert-state nil :background "#73daca" :foreground "black")
-  ;; (set-face-attribute 'doom-modeline-evil-visual-state nil :background "#e0af68" :foreground "black")
-  ;; (set-face-attribute 'doom-modeline-evil-emacs-state nil :background "#bb9af7" :foreground "black")
-  )
+;; (use-package doom-modeline
+;;   :init
+;;   (setq doom-modeline-height 50
+;;         doom-modeline-bar-width 5
+;;         doom-modeline-vcs-max-length 25
+;;         doom-modeline-buffer-file-name-style "file-name")
+;;   :config
+;;   (doom-modeline-mode 1)
+;;   ;; (set-face-attribute 'doom-modeline-time nil :foreground "#e0af68")
+;;   ;; (set-face-attribute 'doom-modeline-evil-normal-state nil :background "#7aa2f7" :foreground "black")
+;;   ;; (set-face-attribute 'doom-modeline-evil-insert-state nil :background "#73daca" :foreground "black")
+;;   ;; (set-face-attribute 'doom-modeline-evil-visual-state nil :background "#e0af68" :foreground "black")
+;;   ;; (set-face-attribute 'doom-modeline-evil-emacs-state nil :background "#bb9af7" :foreground "black")
+;;   )
 
 (use-package solaire-mode
   :defer 1
