bdcaf16a39eaf3467d96b1b62709d74bffb58794
Tyler Steiman <tsteiman@switchboxinc.com>
2023-01-07 17:19:07 -0500

    exwm standalone file for better portability

---
+++ .config/emacs/exwm.el
@@ -0,0 +1,47 @@
+(use-package exwm
+  :config
+  (setq exwm-workspace-number 5)
+  (setq exwm-input-prefix-keys
+        '(?\C-x
+          ?\C-u
+          ?\C-h
+          ?\M-x
+          ;; ?\M-`
+          ?\M-&
+          ?\M-:
+          ;; ?\C-\M-j
+          ;; ?\C-\ 
+          ))
+  (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+  (setq exwm-input-global-keys `(
+                                 ([?\s-d] . (lambda (command)
+                                              (interactive (list (read-shell-command "$ ")))
+                                              (start-process-shell-command command nil command)))
+                                 ([?\s-w] . exwm-workspace-switch)
+                                 ([s-left] . windmove-left)
+                                 ([s-right] . windmove-right)
+                                 ([s-up] . windmove-up)
+                                 ([s-down] . windmove-down)
+                                 ([?\s-h] . windmove-left)
+                                 ([?\s-l] . windmove-right)
+                                 ([?\s-k] . windmove-up)
+                                 ([?\s-j] . windmove-down)
+                                 ([?\s-=] . balance-windows)
+                                 ([?\s-r] . rename-buffer)
+                                 ([?\s-f] . exwm-layout-toggle-fullscreen)
+                                 ([?\s-e] . eshell)
+                                 ,@(mapcar (lambda (i)
+                                             `(,(kbd (format "s-%d" i)) .
+                                               (lambda ()
+                                                 (interactive)
+                                                 (exwm-workspace-switch-create ,i))))
+                                           (number-sequence 0 9))
+                                 ))
+  (add-hook 'exwm-update-class-hook (lambda ()
+                                      (exwm-workspace-rename-buffer (format "*%s*" exwm-class-name))))
+  (require 'exwm-randr)
+  (exwm-randr-enable)
+  (start-process-shell-command "xrandr" nil "xrandr --output eDP-1 --mode 1920x1080 --brightness 0.5 --output HDMI-1-0 --mode 2560x1440 --primary --right-of eDP-1")
+  (start-process-shell-command "feh" nil "feh --bg-scale ~/dot/wallpaper/canvas.jpg")
+  (start-process-shell-command "picom" nil "picom -b")
+  (exwm-enable))
