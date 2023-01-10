f40a5359f701b723bec7b0500721fbea0b1ef866
Tyler Steiman <tsteiman@switchboxinc.com>
2023-01-10 12:39:02 -0500

    emacs compilation mode hook apply colors

--- .config/emacs/modules/my-hooks.el
+++ .config/emacs/modules/my-hooks.el
@@ -1,3 +1,8 @@
 (provide 'my-hooks)
 
+;; prog mode setup hook
 (add-hook 'prog-mode-hook 'my/configure-prog-mode)
+
+;; apply compilation buffer colors so it's not all escaped characters and unreadable
+(add-hook 'compilation-filter-hook (lambda ()
+                                     (ansi-color-apply-on-region (point-min) (point-max))))
