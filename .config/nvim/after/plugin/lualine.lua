c6abf1fc7b3fd11158c17e27b385241f80c471f7
Tyler Steiman <tsteiman@switchboxinc.com>
2023-01-11 13:24:28 -0500

    small nvim updates

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,6 +1,7 @@
 require('lualine').setup {
     options = {
-        section_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
         component_separators = '',
+        icons_enabled = false,
     }
 }
