6f17cd3ccce2a28b1734387f9e158a21201c9665
Tyler Steiman <tsteiman@switchboxinc.com>
2023-01-12 12:25:51 -0500

    install evil collection

--- .config/emacs/modules/my-evil.el
+++ .config/emacs/modules/my-evil.el
@@ -1,37 +1,19 @@
 (provide 'my-evil)
 
-(defun my--evil-key-entry (key func)
-  (evil-define-key nil evil-normal-state-map key func))
-
-(defun my/set-evil-keys ()
-  (my--evil-key-entry " f" 'projectile-find-file)
-  (my--evil-key-entry " s" 'projectile-ripgrep)
-  (my--evil-key-entry " g" 'magit-status)
-  (my--evil-key-entry " l" 'eshell)
-  (my--evil-key-entry " k" 'projectile-compile-project)
-  (my--evil-key-entry " o" 'delete-other-windows)
-  (my--evil-key-entry " j" 'projectile-switch-project))
-
 (use-package evil
-  :config
+  :init
   (setq evil-normal-state-cursor '("#7aa2f7" box))
   (setq evil-insert-state-cursor '("#73daca" (bar . 2)))
   (setq evil-visual-state-cursor '("#e0af68" box))
   (setq evil-emacs-state-cursor '("#bb9af7" box))
-  :hook
-  (prog-mode        . turn-on-evil-mode)
-  (org-mode         . turn-on-evil-mode)
-  (conf-mode        . turn-on-evil-mode)
-  (yaml-mode        . turn-on-evil-mode)
-  (slim-mode        . turn-on-evil-mode)
-  (vterm-mode       . turn-on-evil-mode)
-  (eshell-mode      . turn-on-evil-mode)
-  (git-commit-setup . turn-on-evil-mode)
-  (csv-mode         . turn-on-evil-mode)
-  (help-mode        . turn-on-evil-mode)
-  (helpful-mode     . turn-on-evil-mode)
-  (markdown-mode    . turn-on-evil-mode)
-  (evil-after-load  . my/set-evil-keys))
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :config (evil-collection-init))
 
 (use-package evil-org
   :after org
