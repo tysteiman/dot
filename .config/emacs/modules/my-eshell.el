dce77e4d7c9a787c81ef705afb63d52e48ea682b
Tyler Steiman <tsteiman@switchboxinc.com>
2023-01-15 19:28:26 -0500

    remove unused eshell module

--- .config/emacs/modules/my-eshell.el
+++
@@ -1,9 +0,0 @@
-(provide 'my-eshell)
-
-(use-package eshell-toggle
-  :defer t)
-
-(use-package eshell-git-prompt
-  :after eshell
-  :init (setq eshell-toggle-use-projectile-root t)
-  :config (eshell-git-prompt-use-theme 'multiline2))
