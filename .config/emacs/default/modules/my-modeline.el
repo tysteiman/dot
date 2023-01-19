de10613da92fd14e45e2021d6d4f2cf25fa485c8
Tyler Steiman <tsteiman@switchboxinc.com>
2023-01-19 18:08:23 -0500

    mini modeline with custom format

--- .config/emacs/default/modules/my-modeline.el
+++ .config/emacs/default/modules/my-modeline.el
@@ -1,31 +1,46 @@
 (provide 'my-modeline)
 
-(use-package doom-modeline
-  :init
-  (setq doom-modeline-height 45
-        doom-modeline-bar-width 3
-        doom-modeline-vcs-max-length 25
-        doom-modeline-buffer-file-name-style "file-name")
-  :config
-  (doom-modeline-mode 1)
-  ;; (set-face-attribute 'doom-modeline-time nil :foreground "#e0af68")
-  ;; (set-face-attribute 'doom-modeline-evil-normal-state nil :background "#7aa2f7" :foreground "black")
-  ;; (set-face-attribute 'doom-modeline-evil-insert-state nil :background "#73daca" :foreground "black")
-  ;; (set-face-attribute 'doom-modeline-evil-visual-state nil :background "#e0af68" :foreground "black")
-  ;; (set-face-attribute 'doom-modeline-evil-emacs-state nil :background "#bb9af7" :foreground "black")
-  )
+;; (use-package doom-modeline
+;;   :init
+;;   (setq doom-modeline-height 45
+;;         doom-modeline-bar-width 3
+;;         doom-modeline-vcs-max-length 25
+;;         doom-modeline-buffer-file-name-style "file-name")
+;;   :config
+;;   (doom-modeline-mode 1)
+;;   ;; (set-face-attribute 'doom-modeline-time nil :foreground "#e0af68")
+;;   ;; (set-face-attribute 'doom-modeline-evil-normal-state nil :background "#7aa2f7" :foreground "black")
+;;   ;; (set-face-attribute 'doom-modeline-evil-insert-state nil :background "#73daca" :foreground "black")
+;;   ;; (set-face-attribute 'doom-modeline-evil-visual-state nil :background "#e0af68" :foreground "black")
+;;   ;; (set-face-attribute 'doom-modeline-evil-emacs-state nil :background "#bb9af7" :foreground "black")
+;;   )
 
-(use-package hide-mode-line
-  :hook
-  (vterm-mode  . hide-mode-line-mode)
-  (eshell-mode . hide-mode-line-mode)
-  (org-mode    . hide-mode-line-mode))
-
+;; (use-package hide-mode-line
+;;   :hook
+;;   (vterm-mode  . hide-mode-line-mode)
+;;   (eshell-mode . hide-mode-line-mode)
+;;   (org-mode    . hide-mode-line-mode))
+;; 
 ;; (defun my/format-mode-line (left right)
 ;;   (let* ((available-width (- (window-width) (length left) 2)))
 ;;     (format (format " %%s %%%ds " available-width) left right)))
-;; 
+
 ;; (setq-default mode-line-format
 ;;               '((:eval (my/format-mode-line
 ;;                         (format-mode-line "%b")
 ;;                         (format-mode-line (car (split-string display-time-string)))))))
+
+(defun my/modeline--format ()
+  (require 'vc-git)
+  (require 'projectile)
+  '(:eval (format-mode-line `(,(propertize "%b" 'face '(:foreground "indianred"))
+                              ,(let ((branch (car (vc-git-branches))))
+                                 (when branch
+                                   (propertize (format " [%s:%s]" (projectile-project-name) branch) 'face '(:foreground "MediumPurple"))))
+                              " "
+                              ,(propertize (car (split-string display-time-string)) 'face '(:foreground "CadetBlue"))))))
+
+(use-package mini-modeline
+  :init (setq mini-modeline-r-format (my/modeline--format))
+  :config
+  (mini-modeline-mode))
