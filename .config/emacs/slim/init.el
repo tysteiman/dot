93ef049c6019f55c6d7749efc640f16034433a64
Tyler Steiman <tsteiman@switchboxinc.com>
2023-02-07 09:40:52 -0500

    added to emacs slim conf

--- .config/emacs/slim/init.el
+++ .config/emacs/slim/init.el
@@ -1,6 +1,8 @@
 (setq custom-file (concat user-emacs-directory "custom.el"))
 
-(load-theme 'modus-vivendi t)
+(add-to-list 'default-frame-alist '(font . "JetBrainsMono Nerd Font-11"))
+(set-frame-parameter (selected-frame) 'alpha '(95 90))
+(add-to-list 'default-frame-alist '(alpha 95 90))
 
 ;; set some variables
 (setq make-backup-files nil
@@ -10,7 +12,15 @@
       shr-use-colors nil   ;; eww
       shr-inhibit-images t ;; eww
       shr-use-fonts nil    ;; eww
-      compilation-scroll-output t)
+      compilation-scroll-output t
+      visible-bell nil
+      ring-bell-function 'ignore
+      remote-file-name-inhibit-cache nil
+      tramp-verbose 0
+      vc-ignore-dir-regexp
+      (format "\\(%s\\)\\|\\(%s\\)"
+              vc-ignore-dir-regexp
+              tramp-file-name-regexp))
 
 (setq-default truncate-lines t
 	      indent-tabs-mode nil)
@@ -20,9 +30,13 @@
 (display-time-mode)
 (windmove-default-keybindings)
 (winner-mode)
-(when (display-graphic-p)
-  (tool-bar-mode 0)
-  (scroll-bar-mode 0))
+
+(if (display-graphic-p)
+    (progn
+      (tool-bar-mode 0)
+      (scroll-bar-mode 0))
+  (progn
+    (load-theme 'modus-vivendi t)))
 
 (menu-bar-mode 0)
 
@@ -47,3 +61,8 @@
   "Open Emacs init.el"
   (interactive)
   (find-file (concat user-emacs-directory "init.el")))
+
+(defun work ()
+  "Connect to work via TRAMP"
+  (interactive)
+  (dired "/ssh:work:/home/admin"))
