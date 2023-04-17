ad1e2e7371059690a8397088ff53973fbc86b7cc
Tyler Steiman <tsteiman@switchboxinc.com>
2023-04-17 07:57:37 -0400

    emacs modules

--- .config/emacs/slim/init.el
+++ .config/emacs/slim/init.el
@@ -5,51 +5,15 @@
 
 (add-to-list 'load-path (concat user-emacs-directory "modules"))
 
-;; TODO modules ftw
 (require 'my-conf)
+(require 'my-hooks)
+(require 'my-use-package)
 (require 'my-defuns)
-
-;;; HOOKS
-;; configure prog mode
-(add-hook 'prog-mode-hook (lambda ()
-                            (setq show-trailing-whitespace t)
-                            (hl-line-mode)))
-
-(add-hook 'org-mode-hook (lambda ()
-                           (org-indent-mode)
-                           (setq truncate-lines nil)))
-
-(add-hook 'compilation-filter-hook (lambda ()
-                                     (ansi-color-apply-on-region (point-min) (point-max))))
-
-;;; INSTALL USE-PACKAGE
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
-(unless package-archive-contents
-  (package-refresh-contents))
-
-;; get ready for emacs 29!
-(unless (package-installed-p 'use-package)
-  ;; TODO when looking to emacs 29, if use-package really is built in, we can use that OOTB.
-  (package-install 'use-package))
-
-(require 'use-package)
-
-(setq use-package-always-ensure t)
-;; (setq use-package-verbose t)
+(require 'my-git)
+(require 'my-prettier)
+(require 'my-lsp)
 
 ;;; INSTALL PACKAGES
-(use-package magit
-  :bind (("C-c m" . magit-status)))
-
-(use-package diff-hl
-  :hook
-  (magit-post-refresh . diff-hl-magit-post-refresh)
-  (prog-mode . diff-hl-margin-mode)
-  (prog-mode . diff-hl-mode))
-
 (use-package vterm
   :defer t)
 
@@ -99,19 +63,6 @@
 (use-package company-box
   :hook (company-mode . company-box-mode))
 
-(defun my/configure-prettier ()
-  "Configure Prettier by turning it on only if there is a pretterrc file in the projectile root"
-  (let ((prettier-rc (concat (projectile-project-root) ".prettierrc.json")))
-    (if (file-exists-p prettier-rc)
-        (prettier-mode 1)
-      (message ".prettierrc.json not detected in project root -- skipped loading."))))
-
-(use-package prettier
-  :after (:any js2-mode rjsx-mode)
-  :hook
-  (js2-mode  . my/configure-prettier)
-  (rjsx-mode . my/configure-prettier))
-
 (use-package js2-mode
   :defer t
   :mode "\\.js\\'")
@@ -140,21 +91,6 @@
 (use-package rg
   :commands (projectile-ripgrep))
 
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  (setq lsp-headerline-breadcrumb-enable nil)
-  :config (lsp-enable-which-key-integration t)
-  :hook
-  (typescript-mode . lsp-deferred)
-  (js2-mode        . lsp-deferred)
-  (php-mode        . lsp-deferred))
-
-(use-package lsp-ui
-  :init (setq lsp-ui-doc-position 'at-point)
-  :hook (lsp-mode . lsp-ui-mode))
-
 (use-package doom-modeline
   :init (setq doom-modeline-height 40)
         (setq doom-modeline-buffer-file-name-style "file-name")
@@ -162,7 +98,7 @@
   :config (doom-modeline-mode))
 
 (use-package doom-themes
-  :config (load-theme 'doom-one t))
+  :config (load-theme 'doom-feather-dark t))
 
 (use-package tree-sitter
   :hook
@@ -189,6 +125,14 @@
    :keymaps '(eshell-mode-map vterm-mode-map)
    :states 'normal
    "r" 'rename-buffer)
+
+  ;; SPC+c (Config)
+  (general-define-key
+   :prefix "SPC c"
+   :keymaps 'override
+   :states 'normal
+   "m" 'my/open-module
+   "c" 'open-config)
 
   ;; SPC+g (Git)
   (general-define-key
@@ -232,7 +176,6 @@
    "/" 'swiper-thing-at-point
    "?" 'swiper
    "j" 'projectile-switch-project
-   "m" 'my/open-module
    "M" 'evil-emacs-state
    "s" 'projectile-ripgrep
    "q" 'delete-window
@@ -241,53 +184,12 @@
    "!" 'shell-command
    ";" 'my/comment-current-line
    "," '(execute-extended-command :which-key "M-x")
+   "c" '(:ignore t :which-key "Config")
    "g" '(:ignore t :which-key "Git")
    "k" '(:ignore t :which-key "Bookmarks")))
 
 ;;; KEY BINDINGS
 (global-set-key (kbd "C-x C-b") 'ibuffer) ;; use ibuffer instead of default
-
-;;; DEFUNS
-;; write some defuns
-(defun open-config ()
-  "Open Emacs init.el"
-  (interactive)
-  (find-file (concat user-emacs-directory "init.el")))
-
-(defun my/delete-and-balance-windows ()
-  "Delete current window and balance windows"
-  (interactive)
-  (delete-window)
-  (balance-windows))
-
-(defun my/comment-current-line ()
-  "Comment current line while keeping point the same."
-  (interactive)
-  (save-excursion
-    (comment-line 1)))
-
-(defun my/git-blame-line ()
-  "Run git blame on current line in file"
-  (interactive)
-  (let ((linum (line-number-at-pos))
-        (file (buffer-file-name)))
-    (shell-command (format "git blame -L%s,%s %s" linum linum file) "*Git blame*")))
-
-(defun my/git-blame-file ()
-  "Run git blame on current file"
-  (interactive)
-  (let ((file (buffer-file-name)))
-    (shell-command (format "git blame %s" file) "*Git blame*")))
-
-(defun my/git-blame-region ()
-  "Run git blame on active region"
-  (interactive)
-  (let ((beg (line-number-at-pos (region-beginning)))
-        (end (line-number-at-pos (region-end)))
-        (file (buffer-file-name)))
-    (if (region-active-p)
-        (shell-command (format "git blame -L%s,%s %s" beg end file) "*Git blame*")
-      (message "No active region."))))
 
 ;; Recommended by LSP mode
 (setq gc-cons-threshold 100000000)
---
+++ .config/emacs/slim/modules/my-use-package.el
@@ -0,0 +1,16 @@
+(provide 'my-use-package)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+(setq use-package-always-ensure t)
+;; (setq use-package-verbose t)
