42d3b032be4ae93b88d581dd76f5f8c00a2d09a8
Tyler Steiman <tsteiman@switchboxinc.com>
2023-05-04 10:34:26 -0400

    add some kakoune navigation keys to nvim config

--- .config/nvim/lua/user/keys.lua
+++ .config/nvim/lua/user/keys.lua
@@ -1,3 +1,14 @@
+-- use space for leader key
 vim.g.mapleader = " "
 
+-- use jk instead of escape
 vim.api.nvim_set_keymap('i', 'jk', '<esc>', { noremap = true, silent = true })
+
+-- Kakoune keys
+vim.api.nvim_set_keymap('n', 'gh', '0', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gl', '$', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gi', '^', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gt', 'H', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gb', 'L', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'ge', 'G', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gc', 'M', { noremap = true, silent = true })
