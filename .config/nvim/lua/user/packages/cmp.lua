acc18048f9738137e664cf296445791b76275770
Tyler Steiman <tsteiman@switchboxinc.com>
2023-05-04 20:38:38 -0400

    lua language server

--- .config/nvim/lua/user/packages/cmp.lua
+++ .config/nvim/lua/user/packages/cmp.lua
@@ -66,3 +66,6 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('lspconfig')['tsserver'].setup {
     capabilities = capabilities
 }
+require('lspconfig')['lua_ls'].setup {
+    capabilities = capabilities
+}
--- .config/nvim/lua/user/packages/lsp.lua
+++ .config/nvim/lua/user/packages/lsp.lua
@@ -2,6 +2,7 @@
 local lspconfig = require('lspconfig')
 --lspconfig.pyright.setup {}
 lspconfig.tsserver.setup {}
+lspconfig.lua_ls.setup {}
 --lspconfig.rust_analyzer.setup {
 --    -- Server-specific settings. See `:help lspconfig-setup`
 --    settings = {
