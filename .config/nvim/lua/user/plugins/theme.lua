b20028132c012576cceb610a53bf524938b39513
Tyler Steiman <tsteiman@switchboxinc.com>
2023-05-15 12:14:39 -0400

    nvim use ayu

--- .config/nvim/lua/user/plugins/theme.lua
+++ .config/nvim/lua/user/plugins/theme.lua
@@ -1,9 +1,15 @@
 return {
-    { "Shatur/neovim-ayu", lazy = true, },
-    { "folke/tokyonight.nvim", lazy = true, },
-    { "Shatur/neovim-ayu", lazy = true, },
-    { "ellisonleao/gruvbox.nvim", lazy = true, },
-    { "Mofiqul/vscode.nvim", lazy = true, },
-    { "neanias/everforest-nvim", lazy = true, },
-    { "rose-pine/neovim", lazy = true, name = "rose-pine" },
+    {
+        "Shatur/neovim-ayu",
+        lazy = false,
+        config = function()
+            require('ayu').colorscheme()
+        end
+    },
+    -- { "folke/tokyonight.nvim", lazy = true, },
+    -- { "Shatur/neovim-ayu", lazy = true, },
+    -- { "ellisonleao/gruvbox.nvim", lazy = true, },
+    -- { "Mofiqul/vscode.nvim", lazy = true, },
+    -- { "neanias/everforest-nvim", lazy = true, },
+    -- { "rose-pine/neovim", lazy = true, name = "rose-pine" },
 }
