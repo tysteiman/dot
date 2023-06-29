abbc4937dc56ae5817e9c1fea77e4afc4b0d8dcd
Tyler Steiman <tsteiman@switchboxinc.com>
2023-06-29 16:02:15 -0400

    install gotham theme and rainbow delimiters mode

--- .config/emacs/modules/my-theme.el
+++ .config/emacs/modules/my-theme.el
@@ -3,8 +3,11 @@
 (fringe-mode 2)
 
 ;; download some extra themes even though i don't use them on load
-(use-package doom-themes
-  :defer t)
+(use-package doom-themes :defer t)
+(use-package gotham-theme :defer t)
 
 (use-package hl-todo
   :hook (prog-mode . hl-todo-mode))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
