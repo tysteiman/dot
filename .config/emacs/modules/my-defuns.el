d8a6bf2defc52ea3168e960264ac0aa5d3be0d43
Tyler Steiman <tsteiman@switchboxinc.com>
2023-06-29 13:14:47 -0400

    add docstring to configure prog mode

--- .config/emacs/modules/my-defuns.el
+++ .config/emacs/modules/my-defuns.el
@@ -1,4 +1,5 @@
 (provide 'my-defuns)
 
 (defun my/configure-prog-mode ()
+  "Actions to take whenever prog-mode is entered or other non-prog inherited languages."
   (setq show-trailing-whitespace t))
