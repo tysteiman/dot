9ad4d9d31b83a5bc5436c92bf54d644306b679b4
Tyler Steiman <tsteiman@switchboxinc.com>
2023-07-05 16:40:23 -0400

    install gotham, mood-line

--- .config/emacs/modules/my-theme.el
+++ .config/emacs/modules/my-theme.el
@@ -3,6 +3,13 @@
 (fringe-mode 2)
 
 (use-package doom-themes :defer t)
+(use-package gotham-theme :defer t)
+
+;; (use-package feline
+;;   :config (feline-mode))
+
+(use-package mood-line
+  :config (mood-line-mode))
 
 (use-package hl-todo
   :hook (prog-mode . hl-todo-mode))
