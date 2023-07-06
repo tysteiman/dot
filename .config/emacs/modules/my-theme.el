48878a58eeadf0a0c3928dfd3cf7a5ff5d4a5938
Tyler Steiman <tsteiman@switchboxinc.com>
2023-07-06 13:58:29 -0400

    small emacs theme updates

--- .config/emacs/modules/my-theme.el
+++ .config/emacs/modules/my-theme.el
@@ -2,14 +2,12 @@
 
 (fringe-mode 2)
 
+(set-cursor-color "red")
+(set-background-color "black")
+(set-foreground-color "darkgrey")
+
 (use-package doom-themes :defer t)
 (use-package gotham-theme :defer t)
-
-;; (use-package feline
-;;   :config (feline-mode))
-
-(use-package mood-line
-  :config (mood-line-mode))
 
 (use-package hl-todo
   :hook (prog-mode . hl-todo-mode))
