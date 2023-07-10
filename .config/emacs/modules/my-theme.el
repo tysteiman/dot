e4db0052086e19e98d84e5664e6bce5903fc8308
Tyler Steiman <tsteiman@switchboxinc.com>
2023-07-10 12:58:03 -0400

    dont load emacs theme on startup

--- .config/emacs/modules/my-theme.el
+++ .config/emacs/modules/my-theme.el
@@ -2,7 +2,7 @@
 
 (fringe-mode 5)
 
-(use-package yoshi-theme :config (load-theme 'yoshi t))
+(use-package yoshi-theme :defer t)
 (use-package doom-themes :defer t)
 (use-package gotham-theme :defer t)
 
