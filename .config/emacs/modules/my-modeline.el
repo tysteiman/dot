10cfe9954b07bf4a01988ed03bdfddd71e6a2884
Tyler Steiman <tsteiman@switchboxinc.com>
2023-07-12 12:12:31 -0400

    emacs doom modeline vcs length 30

--- .config/emacs/modules/my-modeline.el
+++ .config/emacs/modules/my-modeline.el
@@ -3,5 +3,6 @@
 (use-package doom-modeline
   :init (setq doom-modeline-height 45
               doom-modeline-icon nil
-              doom-modeline-buffer-file-name-style 'file-name)
+              doom-modeline-buffer-file-name-style 'file-name
+              doom-modeline-vcs-max-length 30)
   :config (doom-modeline-mode t))
