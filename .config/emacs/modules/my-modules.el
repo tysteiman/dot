bf5ba251d43b943c3f24d212645034866862985f
Tyler Steiman <tsteiman@switchboxinc.com>
2023-07-21 16:03:30 -0400

    space out emacs modules, add tab bar

--- .config/emacs/modules/my-modules.el
+++ .config/emacs/modules/my-modules.el
@@ -1,29 +1,51 @@
 (provide 'my-modules)
 
 (require 'my-conf)
+
 (require 'my-defuns)
 
 (require 'my-elpaca)
+
 ;; (require 'my-use-package)
 
 ;; (require 'my-evil)
 
-(require 'my-vertico)
-(require 'my-term)
-(require 'my-langs)
-(require 'my-prettier)
-(require 'my-which-key)
-(require 'my-buffers)
-(require 'my-windows)
-(require 'my-project)
-(require 'my-git)
-(require 'my-org)
-(require 'my-eglot)
-(require 'my-company)
-(require 'my-search)
-(require 'my-tree-sitter)
-(require 'my-theme)
 (require 'my-modeline)
+
+(require 'my-vertico)
+
+(require 'my-term)
+
+(require 'my-langs)
+
+(require 'my-prettier)
+
+(require 'my-which-key)
+
+(require 'my-buffers)
+
+(require 'my-windows)
+
+(require 'my-project)
+
+(require 'my-git)
+
+(require 'my-org)
+
+(require 'my-eglot)
+
+(require 'my-company)
+
+(require 'my-search)
+
+(require 'my-tree-sitter)
+
+(require 'my-theme)
+
 (require 'my-hooks)
+
 (require 'my-keys)
+
 (require 'my-notes)
+
+(require 'my-tab-bar)
