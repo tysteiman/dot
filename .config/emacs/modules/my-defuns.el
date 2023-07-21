3d186692c25e770e3b76078a3395d71fb4fe7812
Tyler Steiman <tsteiman@switchboxinc.com>
2023-07-21 16:03:45 -0400

    hl line mode in prog hook

--- .config/emacs/modules/my-defuns.el
+++ .config/emacs/modules/my-defuns.el
@@ -3,4 +3,5 @@
 (defun my/configure-prog-mode ()
   "Actions to take whenever prog-mode is entered or other non-prog inherited languages."
   (setq show-trailing-whitespace t)
-  (display-line-numbers-mode 1))
+  (display-line-numbers-mode 1)
+  (hl-line-mode 1))
