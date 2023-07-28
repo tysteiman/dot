59ae353c61709d1f1228629c4b4a21c9136682dd
Tyler Steiman <tsteiman@switchboxinc.com>
2023-07-28 11:35:27 -0400

    consult keybindings

--- .config/emacs/modules/my-consult.el
+++ .config/emacs/modules/my-consult.el
@@ -2,4 +2,6 @@
 
 (use-package consult
   :bind (("C-x r j" . consult-register)
-         ("C-M-s" . consult-ripgrep)))
+         ("C-M-s" . consult-ripgrep)
+         ("C-x b" . consult-buffer)
+         ("C-M-y" . consult-yank-from-kill-ring)))
