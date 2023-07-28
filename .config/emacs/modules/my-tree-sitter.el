78ca45ee10764644e99da34d9e03b54bc5309668
Tyler Steiman <tsteiman@switchboxinc.com>
2023-07-28 11:25:21 -0400

    tree sitter for prog mode

--- .config/emacs/modules/my-tree-sitter.el
+++ .config/emacs/modules/my-tree-sitter.el
@@ -2,7 +2,7 @@
 
 (use-package tree-sitter
   :defer t
-  :hook (ruby-mode . tree-sitter-hl-mode))
+  :hook (prog-mode . tree-sitter-hl-mode))
 
 (use-package tree-sitter-langs
   :after tree-sitter
