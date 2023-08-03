4cf037b4940ab67c6df5703910ca7f1d75004e88
Tyler Steiman <tsteiman@switchboxinc.com>
2023-08-03 15:40:52 -0400

    nvim cursorline for cursorword

---
+++ .config/nvim/lua/user/plugins/cursorline.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "yamatsum/nvim-cursorline",
+        lazy = true,
+        event = "BufRead",
+        config = function()
+            require('nvim-cursorline').setup {
+                cursorline = {
+                    enable = false,
+                    -- timeout = 1000,
+                    -- number = false,
+                },
+                cursorword = {
+                    enable = true,
+                    min_length = 3,
+                    hl = { underline = true },
+                }
+            }
+        end,
+    },
+}
