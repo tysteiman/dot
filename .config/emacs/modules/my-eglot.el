43c49d147ec55cb0766d646f177c72c7ed000f4d
Tyler Steiman <tsteiman@switchboxinc.com>
2023-08-24 16:00:16 -0400

    eglot php use intelephense

--- .config/emacs/modules/my-eglot.el
+++ .config/emacs/modules/my-eglot.el
@@ -5,6 +5,8 @@
 
 (use-package eglot
   :defer t
+  :config (with-eval-after-load 'eglot
+            (add-to-list 'eglot-server-programs '(php-mode . ("intelephense" "--stdio"))))
   :bind (("C-c l l" . eglot)
          ("C-c l s" . eglot-shutdown)
          ("C-c l a" . eglot-shutdown-all)))
