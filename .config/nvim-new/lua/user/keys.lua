e2f84e4699c613f4a28b5ef4ada4ebbf149a580e
Tyler Steiman <tyler.steiman@madmobile.com>
2024-03-25 14:45:02 -0400

    remove Ex binding

--- .config/nvim-new/lua/user/keys.lua
+++ .config/nvim-new/lua/user/keys.lua
@@ -3,6 +3,3 @@ vim.g.mapleader = " "
 
 -- use jk instead of escape
 vim.api.nvim_set_keymap('i', 'jk', '<esc>', { noremap = true, silent = true })
-
--- d to enter Ex
-vim.api.nvim_set_keymap('n', '<leader>d', ':Ex<cr>', { noremap = true, silent = true })
