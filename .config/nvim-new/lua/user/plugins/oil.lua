ef55dc8d901980251b73e43f9ae300cdb99c2d72
Tyler Steiman <tyler.steiman@madmobile.com>
2024-03-25 13:48:11 -0400

    nvim oil

---
+++ .config/nvim-new/lua/user/plugins/oil.lua
@@ -0,0 +1,15 @@
+return {
+  'stevearc/oil.nvim',
+  lazy = false,
+  config = function()
+    require("oil").setup({
+      float = {
+        padding = 5,
+        max_width = 75,
+        max_height = 200,
+      }
+    })
+
+    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+  end,
+}
