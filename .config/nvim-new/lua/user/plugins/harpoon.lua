8567e8c1cd1f86e9b32323138f9fd5af5a9261e1
Tyler Steiman <tyler.steiman@madmobile.com>
2024-03-27 11:18:55 -0400

    nvim tokyo, harpoon hh to list

--- .config/nvim-new/lua/user/plugins/harpoon.lua
+++ .config/nvim-new/lua/user/plugins/harpoon.lua
@@ -11,7 +11,7 @@ return {
       -- REQUIRED
 
       vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
-      vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
       vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
       vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
