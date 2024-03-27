6c08c947cbe22e56b8549267ab7326fc7cb4c23b
Tyler Steiman <tyler.steiman@madmobile.com>
2024-03-27 12:31:11 -0400

    nvim lsp-ui

---
+++ .config/nvim-new/lua/user/plugins/lsp-ui.lua
@@ -0,0 +1,44 @@
+return {
+  "jinzhongjia/LspUI.nvim",
+  lazy = true,
+  event = "LspAttach",
+  config = function()
+    require("LspUI").setup({})
+
+    vim.keymap.set('n', '<space>la', function()
+      vim.cmd 'LspUI code_action'
+    end, opts)
+
+    vim.keymap.set('n', '<space>ld', function()
+      vim.cmd 'LspUI definition'
+    end, opts)
+
+    vim.keymap.set('n', '<space>lt', function()
+      vim.cmd 'LspUI type_definition'
+    end, opts)
+
+    vim.keymap.set('n', '<space>li', function()
+      vim.cmd 'LspUI implementation'
+    end, opts)
+
+    vim.keymap.set('n', 'K', function()
+      vim.cmd 'LspUI hover'
+    end, opts)
+
+    vim.keymap.set('n', '<space>ln', function()
+      vim.cmd 'LspUI reference'
+    end, opts)
+
+    vim.keymap.set('n', '<space>lr', function()
+      vim.cmd 'LspUI rename'
+    end, opts)
+
+    vim.keymap.set('n', ']d', function()
+      vim.cmd 'LspUI diagnostic next'
+    end, opts)
+
+    vim.keymap.set('n', '[d', function()
+      vim.cmd 'LspUI diagnostic prev'
+    end, opts)
+  end
+}
--- .config/nvim-new/lua/user/plugins/lsp.lua
+++ .config/nvim-new/lua/user/plugins/lsp.lua
@@ -1,15 +1,4 @@
 return {
-  --{
-  --  "jinzhongjia/LspUI.nvim",
-  --  branch = "legacy",
-  --  lazy = true,
-  --  event = "BufRead",
-  --  config = function()
-  --    require("LspUI").setup({
-  --      prompt = false,
-  --    })
-  --  end
-  --},
   {
     'neovim/nvim-lspconfig',
     lazy = true,
@@ -50,8 +39,8 @@ return {
           local opts = { buffer = ev.buf }
           -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
           -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, opts)
+          -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          -- vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, opts)
           -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
           -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
           -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
@@ -59,9 +48,9 @@ return {
           --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
           -- end, opts)
           -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-          vim.keymap.set('n', '<space>lr', vim.lsp.buf.rename, opts)
-          vim.keymap.set({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', '<leader>ln', vim.lsp.buf.references, opts)
+          -- vim.keymap.set('n', '<space>lr', vim.lsp.buf.rename, opts)
+          -- vim.keymap.set({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, opts)
+          -- vim.keymap.set('n', '<leader>ln', vim.lsp.buf.references, opts)
           vim.keymap.set('n', '<space>lf', function()
             vim.lsp.buf.format { async = true }
           end, opts)
