c33fc9444657103bc31100481cc72a002c6605ce
Tyler Steiman <tyler.steiman@madmobile.com>
2024-03-29 12:54:57 -0400

    nvim surround package

---
+++ .config/nvim/lua/user/plugins/surround.lua
@@ -0,0 +1,11 @@
+return {
+  "kylechui/nvim-surround",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  lazy = true,
+  event = "BufRead",
+  config = function()
+    require("nvim-surround").setup({
+      -- Configuration here, or leave empty to use defaults
+    })
+  end
+}
