1c83eb0cd116d8a5d3bae606a9a469b99dbeff2c
Tyler Steiman <tyler.steiman@madmobile.com>
2024-04-01 15:15:22 -0400

    nvim quickfix keys

--- .config/nvim/lua/user/keys.lua
+++ .config/nvim/lua/user/keys.lua
@@ -3,3 +3,7 @@ vim.g.mapleader = " "
 
 -- use jk instead of escape
 vim.api.nvim_set_keymap('i', 'jk', '<esc>', { noremap = true, silent = true })
+
+-- Quickfix
+vim.api.nvim_set_keymap('n', '<leader>j', '<CMD>cn<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>k', '<CMD>cp<CR>', { noremap = true, silent = true })
