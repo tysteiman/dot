ddbfad33cd7cf83721cf712ad48e85e0a4d064b2
Tyler Steiman <tyler.steiman@madmobile.com>
2024-04-04 14:17:16 -0400

    add wk binding for tsc

--- .config/nvim/lua/user/plugins/tsc.lua
+++ .config/nvim/lua/user/plugins/tsc.lua
@@ -4,5 +4,13 @@ return {
   event = "BufRead *.ts",
   config = function()
     require('tsc').setup()
+
+    local wk = require('which-key')
+    wk.register({
+      c = {
+        name = 'Linting',
+        t = { '<CMD>TSC<CR>', 'Run tsc (TS)' },
+      },
+    }, { prefix = '<leader>' })
   end,
 }
