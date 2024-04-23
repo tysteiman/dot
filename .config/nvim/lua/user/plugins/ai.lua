a25dd3ddd0d3f194dae387e9447f64212fea6d75
Tyler Steiman <tyler.steiman@madmobile.com>
2024-04-23 11:09:05 -0400

    remove nvim gp chat paste kbd

--- .config/nvim/lua/user/plugins/ai.lua
+++ .config/nvim/lua/user/plugins/ai.lua
@@ -14,7 +14,6 @@ return {
         a = { '<CMD>GpChatNew<CR>', 'New GPT Chat' },
         d = { '<CMD>GpChatDelete<CR>', 'Delete GPT Chat' },
         f = { '<CMD>GpChatFinder<CR>', 'Find GPT Chat' },
-        p = { '<CMD>GpChatPaste<CR>', 'Chat Selected Text Into GPT Chat' },
         t = { '<CMD>GpChatToggle<CR>', 'Toggle GPT Chat' },
       },
     }, { prefix = '<leader>' })
