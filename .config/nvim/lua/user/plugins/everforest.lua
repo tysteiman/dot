6367bfd3384181797347ffd03b2c1119d4cae981
Tyler Steiman <tyler.steiman@madmobile.com>
2024-04-26 11:44:04 -0400

    nvim everforest

---
+++ .config/nvim/lua/user/plugins/everforest.lua
@@ -0,0 +1,14 @@
+return {
+  'neanias/everforest-nvim',
+  name = 'everforest',
+  priority = 1000,
+  lazy = false,
+  config = function()
+    require('everforest').setup({
+      -- soft, medium, hard
+      background = "medium",
+    })
+
+    vim.cmd [[ colorscheme everforest ]]
+  end
+}
