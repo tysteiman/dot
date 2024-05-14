fdd6cbc050c3651e01d454c7cc425127609e4b7f
Tyler Steiman <tyler.steiman@madmobile.com>
2024-05-14 16:36:08 -0400

    nvim dapui

--- .config/nvim/lua/user/plugins/dap.lua
+++ .config/nvim/lua/user/plugins/dap.lua
@@ -6,7 +6,30 @@ return {
     dependencies = {"nvim-neotest/nvim-nio"},
     priority = 1,
     config = function()
-      require("dapui").setup()
+      require("dapui").setup({
+        expand_lines = false,
+        layouts = {
+          {
+            elements = {
+              { id = "scopes", size = 0.33 },
+              { id = "breakpoints", size = 0.17 },
+              { id = "stacks", size = 0.25 },
+              { id = "watches", size = 0.25 },
+            },
+            size = 0.33,
+            position = "right",
+          },
+          {
+            elements = {
+              { id = "repl", size = 0.45 },
+              { id = "console", size = 0.55 },
+            },
+            size = 0.27,
+            position = "bottom",
+          },
+        },
+      })
+
       local dap, dapui = require("dap"), require("dapui")
       dap.listeners.before.attach.dapui_config = function()
         dapui.open()
@@ -20,6 +43,14 @@ return {
       dap.listeners.before.event_exited.dapui_config = function()
         dapui.close()
       end
+
+      local wk = require('which-key')
+      wk.register({
+        d = {
+          name = 'Debug',
+          n = { dapui.toggle, 'Open DAP UI', },
+        },
+      }, { prefix = '<leader>' })
     end,
   },
   {
@@ -39,6 +70,35 @@ return {
           runtimeArgs = {
             "run",
             "start:debug",
+          },
+          rootPath = "${workspaceFolder}",
+          cwd = "${workspaceFolder}",
+          console = "integratedTerminal",
+          internalConsoleOptions = "neverOpen",
+        },
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Debug Jest",
+          runtimeExecutable = "npm",
+          runtimeArgs = {
+            "run",
+            "test",
+          },
+          rootPath = "${workspaceFolder}",
+          cwd = "${workspaceFolder}",
+          console = "integratedTerminal",
+          internalConsoleOptions = "neverOpen",
+        },
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Debug Jest (Current File)",
+          runtimeExecutable = "npm",
+          runtimeArgs = {
+            "run",
+            "test",
+            vim.api.nvim_buf_get_name(0),
           },
           rootPath = "${workspaceFolder}",
           cwd = "${workspaceFolder}",
