ea360ee7392f310405d54ffd04b711ad96a68256
Tyler Steiman <tyler.steiman@madmobile.com>
2024-05-22 10:00:43 -0400

    nvim whichkey tab keys

--- .config/nvim/lua/user/plugins/whichkey.lua
+++ .config/nvim/lua/user/plugins/whichkey.lua
@@ -17,6 +17,14 @@ return {
     })
 
     wk.register({
+      b = {
+        name = 'Tabs',
+        b = { function() vim.cmd('tabnew') end, 'New Tab' },
+        n = { function() vim.cmd('tabnext') end, 'Next Tab' },
+        p = { function() vim.cmd('tabprevious') end, 'Previous Tab' },
+        q = { function() vim.cmd('tabclose') end, 'New Tab' },
+        o = { function() vim.cmd('tabonly') end, 'Close All Other Tabs' },
+      },
       p = {
         name = 'Plugins',
         p = { '<CMD>Lazy<CR>', 'Lazy' },
