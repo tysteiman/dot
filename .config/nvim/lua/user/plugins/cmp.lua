b987943e19ac3a7369229c66793ce4e239822473
Tyler Steiman <tyler.steiman@madmobile.com>
2024-05-28 13:44:05 -0400

    nvim try out codeium

--- .config/nvim/lua/user/plugins/cmp.lua
+++ .config/nvim/lua/user/plugins/cmp.lua
@@ -32,7 +32,8 @@ return {
       }),
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'luasnip' }, -- For luasnip users.
+        { name = 'luasnip' },
+        { name = 'codeium' },
       }, {
         { name = 'buffer' },
       })
---
+++ .config/nvim/lua/user/plugins/codeium.lua
@@ -0,0 +1,12 @@
+return {
+  "Exafunction/codeium.nvim",
+  lazy = true,
+  event = 'BufRead',
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    require("codeium").setup()
+  end,
+}
