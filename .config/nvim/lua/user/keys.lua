4f1072139aef07f106cd9a676cedacdf647c70a1
Tyler Steiman <tyler.steiman@madmobile.com>
2024-06-24 10:35:00 -0400

    nvim keys

--- .config/nvim/lua/user/keys.lua
+++ .config/nvim/lua/user/keys.lua
@@ -7,3 +7,9 @@ vim.api.nvim_set_keymap('i', 'jk', '<esc>', { noremap = true, silent = true })
 -- Quickfix
 vim.api.nvim_set_keymap('n', '<leader>j', '<CMD>cn<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>k', '<CMD>cp<CR>', { noremap = true, silent = true })
+
+-- Window navigation
+vim.api.nvim_set_keymap('n', '<c-h>', '<CMD>wincmd h<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<c-l>', '<CMD>wincmd l<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<c-k>', '<CMD>wincmd k<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<c-j>', '<CMD>wincmd j<CR>', { noremap = true, silent = true })
