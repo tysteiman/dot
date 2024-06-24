77683b08276a8082df7d654ee77560defd8ae1d2
Tyler Steiman <tyler.steiman@madmobile.com>
2024-06-24 10:34:46 -0400

    dap comment

--- .config/nvim/lua/user/plugins/dap.lua
+++ .config/nvim/lua/user/plugins/dap.lua
@@ -154,29 +154,29 @@ return {
 -- local dap = require('dap')
 -- 
 -- dap.adapters.php = {
---   type = 'executable',
---   command = 'node',
---   args = { '/path/to/vscode-php-debug/out/phpDebug.js' }
--- }
--- 
--- dap.configurations.php = {
---   {
---     type = 'php',
---     request = 'launch',
---     name = 'Listen for Xdebug',
---     port = 9000,
---     stopOnEntry = true,
---   },
--- }
--- 
--- dap.configurations.php = dap.configurations.php or {}
--- table.insert(dap.configurations.php, {
---   type = 'php',
---   request = 'launch',
---   name = 'Launch currently open script',
---   program = '${file}',
---   cwd = vim.fn.getcwd(),
---   port = 9000,
--- })
+  --   type = 'executable',
+  --   command = 'node',
+  --   args = { '/path/to/vscode-php-debug/out/phpDebug.js' }
+  -- }
+  -- 
+  -- dap.configurations.php = {
+    --   {
+      --     type = 'php',
+      --     request = 'launch',
+      --     name = 'Listen for Xdebug',
+      --     port = 9000,
+      --     stopOnEntry = true,
+      --   },
+      -- }
+      -- 
+      -- dap.configurations.php = dap.configurations.php or {}
+      -- table.insert(dap.configurations.php, {
+        --   type = 'php',
+        --   request = 'launch',
+        --   name = 'Launch currently open script',
+        --   program = '${file}',
+        --   cwd = vim.fn.getcwd(),
+        --   port = 9000,
+        -- })
 
--- VSCODE PHP INSTALL: https://github.com/xdebug/vscode-php-debug
+        -- VSCODE PHP INSTALL: https://github.com/xdebug/vscode-php-debug
