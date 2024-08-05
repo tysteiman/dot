22441a7123ca94779ede6f3e81340ffe6e6abd06
Tyler Steiman <tyler.steiman@madmobile.com>
2024-08-05 10:47:04 -0400

    use a more default telescope

--- .config/nvim/lua/user/plugins/telescope.lua
+++ .config/nvim/lua/user/plugins/telescope.lua
@@ -12,32 +12,43 @@ return {
         name = 'Find',
         f = { builtin.git_files, 'Git Files' },
         F = { builtin.find_files, 'All Files' },
-        g = { function()
-          builtin.live_grep(themes.get_ivy({}))
-        end, 'Grep' },
+        g = { builtin.live_grep, 'Grep' },
+        -- g = { function()
+        --   builtin.live_grep(themes.get_ivy({}))
+        -- end, 'Grep' },
         b = { builtin.buffers, 'Buffers' },
       },
       l = {
         name = 'LSP',
         s = { builtin.lsp_document_symbols, 'Document Symbols' },
         S = { builtin.lsp_workspace_symbols, 'Workspace Symbols' },
+        N = { builtin.lsp_references, 'References (Telescope)' },
       },
     }, { prefix = '<leader>' })
 
     local telescope = require('telescope')
-
-    telescope.setup {
+    telescope.setup({
       defaults = {
-        layout_strategy = "center",
         layout_config = {
-          width = 0.4,
-          height = 0.3,
+          width = 0.9,
           prompt_position = "top",
         },
-        preview = false,
-        previewer = false,
         sorting_strategy = "ascending",
       }
-    }
+    })
+
+    -- telescope.setup {
+    --   defaults = {
+    --     layout_strategy = "center",
+    --     layout_config = {
+    --       width = 0.4,
+    --       height = 0.3,
+    --       prompt_position = "top",
+    --     },
+    --     preview = false,
+    --     previewer = false,
+    --     sorting_strategy = "ascending",
+    --   }
+    -- }
   end,
 }
