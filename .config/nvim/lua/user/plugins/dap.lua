496961c678fa33240317de2f171393e88c11169e
Tyler Steiman <tyler.steiman@madmobile.com>
2024-08-15 14:44:09 -0400

    move vscode php debug into own file config block

--- .config/nvim/lua/user/plugins/dap.lua
+++ .config/nvim/lua/user/plugins/dap.lua
@@ -9,12 +9,6 @@ return {
   config = function()
     local dap = require('dap')
 
-    dap.adapters.php = {
-      type = 'executable',
-      command = 'node',
-      args = {os.getenv('HOME') .. '/.local/share/nvim/lazy/vscode-php-debug/out/phpDebug.js'}
-    }
-
     dap.configurations.php = {
       {
         type = 'php',
--- .config/nvim/lua/user/plugins/vscode-php-debug.lua
+++ .config/nvim/lua/user/plugins/vscode-php-debug.lua
@@ -3,4 +3,12 @@ return {
   lazy = true,
   event = "BufEnter *.php",
   priority = 2,
+  config = function()
+    local dap = require('dap')
+    dap.adapters.php = {
+      type = 'executable',
+      command = 'node',
+      args = {os.getenv('HOME') .. '/.local/share/nvim/lazy/vscode-php-debug/out/phpDebug.js'}
+    }
+  end,
 }
