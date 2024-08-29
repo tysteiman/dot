f4e30eabf159a3ff90ef37e736b03925734e36d7
Tyler Steiman <tyler.steiman@madmobile.com>
2024-08-29 14:18:10 -0400

    nvim lualine update

--- .config/nvim/lua/user/plugins/lualine.lua
+++ .config/nvim/lua/user/plugins/lualine.lua
@@ -5,9 +5,7 @@ return {
   config = function()
     require('lualine').setup {
       options = {
-        -- section_separators = { left = '', right = '' },
-        -- section_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
       },
     }
