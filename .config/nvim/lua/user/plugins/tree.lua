2e266633817fe3920d0432f159585639905176bc
Tyler Steiman <tyler.steiman@madmobile.com>
2024-09-16 13:24:42 -0400

    nvim tree

---
+++ .config/nvim/lua/user/plugins/tree.lua
@@ -0,0 +1,11 @@
+return {
+  'nvim-tree/nvim-tree.lua',
+  config = function()
+    require("nvim-tree").setup({
+      view = {
+        width = 45,
+      },
+    })
+    vim.api.nvim_set_keymap('n', '<leader>e', '<CMD>NvimTreeFindFileToggle<CR>', { noremap = true, silent = true })
+  end,
+}
