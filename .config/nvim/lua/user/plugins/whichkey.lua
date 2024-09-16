6609962a6bc7a2eed9785df2739a6a4356993a17
Tyler Steiman <tyler.steiman@madmobile.com>
2024-09-16 13:24:48 -0400

    nvim whichkey barbar

--- .config/nvim/lua/user/plugins/whichkey.lua
+++ .config/nvim/lua/user/plugins/whichkey.lua
@@ -24,6 +24,7 @@ return {
       b = {
         name = 'Tabs',
         b = { function() vim.cmd('tabnew') end, 'New Tab' },
+        B = { function() require('barbar') end, 'Enable Tab Bar' },
         n = { function() vim.cmd('tabnext') end, 'Next Tab' },
         p = { function() vim.cmd('tabprevious') end, 'Previous Tab' },
         q = { function() vim.cmd('tabclose') end, 'New Tab' },
