8d8964739e476a24f573b60355ee3883293e2750
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-11 12:44:26 -0400

    small nvim updates

--- .config/nvim/lua/user/plugins/hl-colors.lua
+++ .config/nvim/lua/user/plugins/hl-colors.lua
@@ -1,7 +1,10 @@
 return {
   'brenoprata10/nvim-highlight-colors',
   lazy = true,
-  event = {'BufEnter *.css', 'BufEnter *.scss'},
+  event = {
+    'BufEnter *.css',
+    'BufEnter *.scss',
+  },
   config = function()
     require('nvim-highlight-colors').setup()
   end,
