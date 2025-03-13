4e2f7c485b986218ddd50584c10ba8fc58d916df
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-13 10:50:57 -0400

    remove unsed nvim packages after testing

--- .config/nvim/lua/user/plugins/lsp-ui.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'jinzhongjia/LspUI.nvim',
-  event = 'LspAttach',
-  opts = {},
-  keys = {
-    { '<leader>la', '<cmd>LspUI code_action<cr>', desc = 'Code Action' },
-    { '<leader>lr', '<cmd>LspUI rename<cr>', desc = 'Rename' },
-  },
-}
