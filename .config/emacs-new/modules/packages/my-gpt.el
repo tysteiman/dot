1c7a2dd18b0c9f3079c0d3ec4ef826668c086f1a
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-17 17:09:02 -0400

    emacs gpt binds

--- .config/emacs-new/modules/packages/my-gpt.el
+++ .config/emacs-new/modules/packages/my-gpt.el
@@ -2,7 +2,13 @@
 
 (use-package gptel
   :defer t
-  :init (let ((keyfile "~/.gptel.el"))
+  :init (let ((keyfile "~/.gpt.el"))
           (when (file-exists-p keyfile)
             (load-file keyfile)
-            (setq gptel-api-key 'my--gptel-key))))
+            (setq gptel-api-key 'my--gptel-key)))
+  :bind (("C-c a a" . 'gptel)
+         ("C-c a s" . 'gptel-send)
+         ("C-c a c" . 'gptel-add)
+         ("C-c a f" . 'gptel-add-file)
+         ("C-c a q" . 'gptel-context-remove-all)
+         ("C-c a r" . 'gptel-rewrite)))
