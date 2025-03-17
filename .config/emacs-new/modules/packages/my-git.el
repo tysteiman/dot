3976a8f693023530167aba4e29e7e66b273cba89
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-17 11:42:13 -0400

    emacs git package module

---
+++ .config/emacs-new/modules/packages/my-git.el
@@ -0,0 +1,10 @@
+(provide 'my-git)
+
+(use-package magit :defer t)
+
+(use-package diff-hl
+  :hook
+  (prog-mode . diff-hl-mode)
+  (yaml-mode . diff-hl-mode)
+  (magit-pre-refresh . diff-hl-magit-pre-refresh)
+  (magit-post-refresh . diff-hl-magit-post-refresh))
