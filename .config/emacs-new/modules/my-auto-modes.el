51244c97b8b196a4efba24baa5e06740ebd32ed2
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-17 16:13:07 -0400

    Emacs set js-jsx mode to .[tj]sx files

--- .config/emacs-new/modules/my-auto-modes.el
+++ .config/emacs-new/modules/my-auto-modes.el
@@ -1,5 +1,5 @@
 (provide 'my-auto-modes)
 
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . js-jsx-mode))
+(add-to-list 'auto-mode-alist '("\\.[tj]sx\\'" . js-jsx-mode))
 
 (add-to-list 'auto-mode-alist '("\\.env.*\\'" . conf-mode))
