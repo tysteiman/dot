f6a047ec314776914097a1210beeaf15dd290329
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-17 14:49:50 -0400

    Add more to emacs langs packages

--- .config/emacs-new/modules/packages/my-langs.el
+++ .config/emacs-new/modules/packages/my-langs.el
@@ -3,3 +3,9 @@
 (use-package typescript-mode :defer t)
 
 (use-package yaml-mode :defer t)
+
+(use-package web-mode :defer t)
+
+(use-package dockerfile-mode :defer t)
+
+(use-package php-mode :defer t)
