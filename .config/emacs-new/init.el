19a2d5780e7a1dcbb41534c3c118269ff555e469
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-17 12:06:00 -0400

    load custom file on startup

--- .config/emacs-new/init.el
+++ .config/emacs-new/init.el
@@ -1,4 +1,7 @@
-(setq custom-file (concat user-emacs-directory "custom.el"))
+(let ((custom (concat user-emacs-directory "custom.el")))
+  (setq custom-file custom)
+  (when (file-exists-p custom)
+    (load-file custom)))
 
 (add-to-list 'load-path (concat user-emacs-directory "modules"))
 (add-to-list 'load-path (concat user-emacs-directory "modules" "/packages"))
