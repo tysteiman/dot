4afbaa48f30b56ef35b5e0a4345a6e6f657a454a
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-17 13:03:08 -0400

    lsp perf stuff in init.el

--- .config/emacs-new/init.el
+++ .config/emacs-new/init.el
@@ -1,11 +1,18 @@
+;; LSP settings: https://emacs-lsp.github.io/lsp-mode/page/performance/
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024))
+
+;; Configure 'custom' file
 (let ((custom (concat user-emacs-directory "custom.el")))
   (setq custom-file custom)
   (when (file-exists-p custom)
     (load-file custom)))
 
+;; Configure module paths
 (add-to-list 'load-path (concat user-emacs-directory "modules"))
 (add-to-list 'load-path (concat user-emacs-directory "modules" "/packages"))
 
+;; Load config modules
 (require 'my-settings)
 (require 'my-keys)
 (require 'my-hooks)
