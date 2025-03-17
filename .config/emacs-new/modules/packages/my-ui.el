55c0048f89428ab10e501092f9dc0608c345e109
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-17 16:30:56 -0400

    doom modeline

--- .config/emacs-new/modules/packages/my-ui.el
+++ .config/emacs-new/modules/packages/my-ui.el
@@ -4,10 +4,15 @@
 
 (use-package doom-themes :defer t)
 
-(use-package gruvbox-theme :defer t)
-
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package diredfl
   :hook (dired-mode . diredfl-mode))
+
+(use-package doom-modeline
+  :init (setq doom-modeline-height 30
+              doom-modeline-vcs-max-length 60
+              doom-modeline-buffer-file-name-style "file-name"
+              doom-modeline-time-icon nil)
+  :config (doom-modeline-mode t))
