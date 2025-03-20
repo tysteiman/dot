847acd81785c855b7ef0d5491a74d52c06eb0640
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-20 14:41:10 -0400

    ui.el add bindings for toggling ef random themes

--- .config/emacs-new/modules/packages/my-ui.el
+++ .config/emacs-new/modules/packages/my-ui.el
@@ -1,6 +1,19 @@
 (provide 'my-ui)
 
-(use-package ef-themes :defer t)
+(defun my--load-dark-random ()
+  "Load a random dark theme"
+  (interactive)
+  (ef-themes-load-random 'dark))
+
+(defun my--load-light-random ()
+  "Load a random light theme"
+  (interactive)
+  (ef-themes-load-random 'light))
+
+(use-package ef-themes
+  :bind
+  (("C-c u d" . my--load-dark-random)
+   ("C-c u l" . my--load-light-random)))
 
 (use-package doom-themes :defer t)
 
@@ -10,9 +23,9 @@
 (use-package diredfl
   :hook (dired-mode . diredfl-mode))
 
-(use-package doom-modeline
-  :init (setq doom-modeline-height 30
-              doom-modeline-vcs-max-length 60
-              doom-modeline-buffer-file-name-style "file-name"
-              doom-modeline-time-icon nil)
-  :config (doom-modeline-mode t))
+;; (use-package doom-modeline
+;;   :init (setq doom-modeline-height 30
+;;               doom-modeline-vcs-max-length 60
+;;               doom-modeline-buffer-file-name-style "file-name"
+;;               doom-modeline-time-icon nil)
+;;   :config (doom-modeline-mode t))
