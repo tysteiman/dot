b970204b46f7b071e6ed97596ac207697ae0f6ff
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-21 14:40:47 -0400

    Add docstring to my--swiper

--- .config/emacs-new/modules/packages/my-search.el
+++ .config/emacs-new/modules/packages/my-search.el
@@ -1,6 +1,8 @@
 (provide 'my-search)
 
 (defun my--swiper (&optional arg)
+  "Call either `swiper' or `swiper-thing-at-point' depending on if a prefix was passed.
+When a prefix is passed (C-u), `swiper-thing-at-point' is used, otherwise just `swiper'."
   (interactive "P")
   (if current-prefix-arg
       (swiper-thing-at-point)
