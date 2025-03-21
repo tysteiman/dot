1dddbdbc5fe3ef2012c4c8bd3b0a0029d8da989d
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-21 14:38:22 -0400

    emacs my--swiper to check prefix arg for swiper-thing-at-point or just swiper

--- .config/emacs-new/modules/packages/my-search.el
+++ .config/emacs-new/modules/packages/my-search.el
@@ -1,4 +1,10 @@
 (provide 'my-search)
 
+(defun my--swiper (&optional arg)
+  (interactive "P")
+  (if current-prefix-arg
+      (swiper-thing-at-point)
+    (swiper)))
+
 (use-package swiper
-  :bind ("C-M-/" . swiper))
+  :bind ("C-M-/" . my--swiper))
