71800cdc9cc0ce0fdebfbbe1631e3f72c9823ad5
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-24 13:34:49 -0400

    dashboard key, use banner.txt

--- .config/emacs-new/modules/packages/my-dashboard.el
+++ .config/emacs-new/modules/packages/my-dashboard.el
@@ -1,10 +1,12 @@
 (provide 'my-dashboard)
 
 (use-package dashboard
+  :demand t
   :init
-  (setq dashboard-startup-banner 3
+  (setq dashboard-startup-banner (concat user-emacs-directory "banner.txt")
         dashboard-items '((recents . 10)
                           (bookmarks . 10)
                           (projects . 10)))
   :config
-  (dashboard-setup-startup-hook))
+  (dashboard-setup-startup-hook)
+  :bind (("C-c o d" . dashboard-open)))
