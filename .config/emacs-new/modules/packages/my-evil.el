bcc13ccdf94834a70f6b4bf843959882c41f3561
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-26 13:24:17 -0400

    emacs evil-escape package

--- .config/emacs-new/modules/packages/my-evil.el
+++ .config/emacs-new/modules/packages/my-evil.el
@@ -5,3 +5,7 @@
 
 (use-package evil-surround
   :hook (evil-mode . evil-surround-mode))
+
+(use-package evil-escape
+  :init (setq evil-escape-key-sequence "jk")
+  :hook (evil-mode . evil-escape-mode))
