32963845cf2f19d67fca6f60f28908dd4c16ee87
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-26 13:10:58 -0400

    emacs modeline & solaire

--- .config/emacs-new/modules/packages/my-ui.el
+++ .config/emacs-new/modules/packages/my-ui.el
@@ -15,7 +15,8 @@
   (("C-c u d" . my/load-dark-random)
    ("C-c u l" . my/load-light-random)))
 
-(use-package doom-themes :defer t)
+(use-package doom-themes
+  :config (load-theme 'doom-feather-dark t))
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
@@ -23,9 +24,12 @@
 (use-package diredfl
   :hook (dired-mode . diredfl-mode))
 
-;; (use-package doom-modeline
-;;   :init (setq doom-modeline-height 30
-;;               doom-modeline-vcs-max-length 60
-;;               doom-modeline-buffer-file-name-style "file-name"
-;;               doom-modeline-time-icon nil)
-;;   :config (doom-modeline-mode t))
+(use-package solaire-mode
+  :config (solaire-global-mode 1))
+
+(use-package doom-modeline
+  :init (setq doom-modeline-height 30
+              doom-modeline-vcs-max-length 60
+              doom-modeline-buffer-file-name-style "file-name"
+              doom-modeline-time-icon nil)
+  :config (doom-modeline-mode t))
