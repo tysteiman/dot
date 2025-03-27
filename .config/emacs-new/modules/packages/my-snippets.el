c6a796071b0235ee5cde63ebcafd405229196bb4
Tyler Steiman <tyler.steiman@madmobile.com>
2025-03-27 09:52:46 -0400

    Emacs use banner-comment with C-u C-x C-;

--- .config/emacs-new/modules/packages/my-snippets.el
+++ .config/emacs-new/modules/packages/my-snippets.el
@@ -5,3 +5,13 @@
   :hook
   (html-mode . (lambda () (keymap-set html-mode-map "C-j" 'emmet-expand-line)))
   (js-jsx-mode . (lambda () (keymap-set js-jsx-mode-map "C-j" 'emmet-expand-line))))
+
+(use-package banner-comment
+  :bind (("C-x C-;" . my--comment)))
+
+(defun my--comment (&optional arg)
+  "Call either `comment-line' or `banner-comment' based on if PREFIX was sent."
+  (interactive "P")
+  (if current-prefix-arg
+      (banner-comment)
+    (comment-line 1)))
