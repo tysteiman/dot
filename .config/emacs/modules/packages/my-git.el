fea6db61eed75ad6312fc4af0d4d3a5dc773ec1f
Tyler Steiman <tsteiman@switchboxinc.com>
2025-04-15 14:15:03 -0400

    magit C-c m, conf mode use diff-hl-mode

--- .config/emacs/modules/packages/my-git.el
+++ .config/emacs/modules/packages/my-git.el
@@ -1,11 +1,13 @@
 (provide 'my-git)
 
-(use-package magit :defer t)
+(use-package magit
+  :bind (("C-c m" . magit-status)))
 
 (use-package diff-hl
   :hook
   (prog-mode . diff-hl-mode)
   (yaml-mode . diff-hl-mode)
   (nxml-mode . diff-hl-mode)
+  (conf-mode . diff-hl-mode)
   (magit-pre-refresh . diff-hl-magit-pre-refresh)
   (magit-post-refresh . diff-hl-magit-post-refresh))
