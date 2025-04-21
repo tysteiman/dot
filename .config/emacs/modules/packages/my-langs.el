8b77a6a82607027ae45ec9b97512c72db309073c
Tyler Steiman <tsteiman@switchboxinc.com>
2025-04-21 15:04:35 -0400

    install nginx lang, yaml show trailing

--- .config/emacs/modules/packages/my-langs.el
+++ .config/emacs/modules/packages/my-langs.el
@@ -2,10 +2,15 @@
 
 (use-package typescript-mode :defer t)
 
-(use-package yaml-mode :defer t)
+(use-package yaml-mode
+  :defer t
+  :hook (yaml-mode . (lambda ()
+                       (setq show-trailing-whitespace t))))
 
 (use-package web-mode :defer t)
 
 (use-package dockerfile-mode :defer t)
 
 (use-package php-mode :defer t)
+
+(use-package nginx-mode :defer t)
